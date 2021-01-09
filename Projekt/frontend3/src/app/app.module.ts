afab6689337035cb28bde6704c7f4e08fc2f6a2d
cagla01 <caglak770@gmail.com>
2021-01-09 13:00:07 +0100

    Übung 9

---
+++ Projekt/frontend3/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CreateComponent } from './members/create/create.component';
+import { ReadComponent } from './members/read/read.component';
+import { UpdateComponent } from './members/update/update.component';
+import { DeleteComponent } from './members/delete/delete.component';
+import {HttpClientModule} from "@angular/common/http";
+import { NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { allIcons } from 'ngx-bootstrap-icons';
+import { FormComponent } from './members/read/form/form.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CreateComponent,
+    ReadComponent,
+    UpdateComponent,
+    DeleteComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
